994fc02247618e58d6fca4c21e6907c39a6b6429
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:29:20 +0200

    add app

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+using ConsoleUI;
+
+PersonModel person = new PersonModel("Corey");
+
+person.FirstName = "Tim";
+//person.LastName = "Corey";
+person.Age = 40;
+person.SSN = "123-45-6789";
+
+Console.WriteLine(person.FullName);
+
+Console.WriteLine(person.SSN);
+
+Console.ReadLine();
